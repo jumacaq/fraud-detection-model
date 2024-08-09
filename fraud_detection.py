6f0f6aacdb09116b00a5786c48201d8afb64dc14
Juan Campos Quintana <67085586+jumacaq@users.noreply.github.com>
2024-08-09 09:42:22 -0500

    Update fraud_detection.py

--- fraud_detection.py
+++ fraud_detection.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import sklearn
-import timeit
+#import timeit
 import warnings
 warnings.filterwarnings("ignore")
 import matplotlib.pyplot as plt
-import seaborn as sns
-import os
+#import seaborn as sns
+
 
 
 
